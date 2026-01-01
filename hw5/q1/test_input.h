cf49bf62047c7a95cadcc7fb1f5fcd947997ffd3
yusuf <81180829+yusufsavm@users.noreply.github.com>
2026-01-02 00:48:54 +0300

    Add files via upload

---
+++ hw5/q1/test_input.h
@@ -0,0 +1,19 @@
+
+// Bu dosya create_real_input.py tarafindan olusturuldu.
+// Kaynak ses dosyasi: 0_jackson_0.wav
+// Beklenen Tahmin: 0 (Sifir)
+
+#ifndef TEST_INPUT_H
+#define TEST_INPUT_H
+
+// Yapay zeka modelinin girisine kopyalanacak 26 adet MFCC degeri:
+const float test_input_mfcc[26] = {
+    -145.776367f,     36.124279f,     6.816034f,     8.517128f,     -0.846540f, 
+    -3.859671f,     -0.653585f,     -3.257571f,     -2.766927f,     -0.594754f, 
+    -0.024657f,     -0.212580f,     1.744763f,     0.373188f,     0.569628f, 
+    -0.749480f,     -0.123376f,     0.455365f,     0.120192f,     -0.031725f, 
+    0.101229f,     0.271682f,     0.115815f,     -0.257122f,     0.093914f, 
+    -0.184130f, 
+};
+
+#endif // TEST_INPUT_H
