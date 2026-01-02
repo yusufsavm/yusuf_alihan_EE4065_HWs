eaccebec9a1f862b233d8f180fac7b01b891ed00
yusuf <81180829+yusufsavm@users.noreply.github.com>
2026-01-03 00:05:43 +0300

    Add files via upload

---
+++ hw5/q2/test_input_image.h
@@ -0,0 +1,17 @@
+#ifndef TEST_INPUT_IMAGE_H
+#define TEST_INPUT_IMAGE_H
+
+// Dizi isminin asagidaki gibi "test_input_image" olmasi SART!
+// Cunku app_x-cube-ai.c dosyasinda bu isimle cagiriyoruz.
+
+const float test_input_image[7] = {
+    1.521293f,
+    0.127718f,
+    3.743706f,
+    1.699587f,
+    -0.116587f,
+    0.170536f,
+    -1.381844f
+};
+
+#endif // TEST_INPUT_IMAGE_H
